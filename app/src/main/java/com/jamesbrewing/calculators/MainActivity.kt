05f69a200e32a813c686bf0f4defc6a45bbf2aa1
Jeff James <jeffjames@gmail.com>
2021-10-13 12:11:41 -0700

    Add onClickListener programmatically

--- app/src/main/java/com/jamesbrewing/calculators/MainActivity.kt
+++ app/src/main/java/com/jamesbrewing/calculators/MainActivity.kt
@@ -3,7 +3,11 @@ package com.jamesbrewing.calculators
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import android.view.View
+import android.widget.Button
+import android.widget.EditText
 
 //const val EXTRA_MESSAGE = "com.example.myfirstapp.MESSAGE"
 
@@ -11,6 +15,16 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val ogfgButton: Button = findViewById(R.id.buttonOGFG)
+        ogfgButton.setOnClickListener {
+            calcABVOGFG(ogfgButton)
+        }
+
+        val sgBrixButton: Button = findViewById(R.id.buttonSGBrix)
+        sgBrixButton.setOnClickListener {
+            calcABVOGFG(sgBrixButton)
+        }
     }
 
     fun calcABVOGFG(view: View) {
